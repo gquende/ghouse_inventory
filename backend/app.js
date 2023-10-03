cd4eb5fcedbe7b5203655dc46e4e3b441b87c3c2
Geraldo Quende <43283373+gquende@users.noreply.github.com>
2023-10-03 16:27:08 +0100

    Feat: Show Homeview

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,12 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const homepage = __dirname + "/src/views/index.html";
 
-const db = mysql.createConnection({
-  host: 'mysql', 
-  user: 'root',
-  password: 'password', 
-  database: 'mydb', 
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao MySQL: ' + err.message);
-  } else {
-    console.log('Conexão bem-sucedida ao MySQL');
-  }
-});
-
-app.get('/', (req, res) => {
-  db.query('SELECT * FROM users', (err, result) => {
-    if (err) {
-      res.send('Erro ao buscar dados do MySQL: ' + err.message);
-    } else {
-      res.json(result);
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(homepage);
 });
 
 app.listen(port, () => {
